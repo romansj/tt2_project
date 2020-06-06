b604e12fe3690e4e4034237ed2168c6a0b04cfbb
Jānis Romāns <30439620+romansj@users.noreply.github.com>
2020-06-06 19:35:22 +0300

    added main page and favicon

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'amachef.apps.AmachefConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
