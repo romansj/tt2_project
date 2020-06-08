ac0fd8e9f27e2c19806ff8b23dfbe393cf9b6398
Elvis <elvis.stapkevics@gmail.com>
2020-06-08 17:35:08 +0300

    Pievienota vienkārša funkcionalitāte kalkulatoram.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '(dhw@sih_%0nhub%yur&csj2_o1!d-y^ndgx69t!+2-pps)zls'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 LOGIN_REDIRECT_URL = "/users/"
