5334438b862572f03c450865895c4c636a7f3211
Jānis Romāns <30439620+romansj@users.noreply.github.com>
2020-06-11 15:06:59 +0300

    filter implementation

--- mysite/search/views.py
+++ mysite/search/views.py
@@ -1,3 +1,5 @@
+from django.core.paginator import Paginator
+from django.db.models import Q
 from django.http import JsonResponse
 from django.shortcuts import render
 # Create your views here.
@@ -12,28 +14,35 @@ def index(request):
 
 def recipe_results(request):
     # todo pagaidam 2
-    paginate_by = 2
 
     url_parameter = request.GET.get("q")
+    url_parameter_cat = request.GET.get("cat")
 
-    if url_parameter:
-
+    if url_parameter and not url_parameter_cat:
         recipes = Post.objects.filter(title__icontains=url_parameter)
+    elif url_parameter_cat and not url_parameter:
+        print(url_parameter_cat, int(url_parameter_cat))
+        recipes = Post.objects.filter(category_new_id=int(url_parameter_cat))
+    elif url_parameter and url_parameter_cat:
+        criterion1 = Q(title__icontains=url_parameter)
+        criterion2 = Q(category_new_id=int(url_parameter_cat))
+        recipes = Post.objects.filter(criterion2 & criterion1)  # var ari '(title__icontains=url_parameter, category_new_id=int(url_parameter_cat)) ieks filter'
     else:
         recipes = Post.objects.all()
 
     categories = Category.objects.all()
     ingredients = Ingredient.objects.all()
 
-    ctxt = {}
-    ctxt["recipes"] = recipes
-    ctxt["categories"] = categories
-    ctxt["ingredients"] = ingredients
+    paginator = Paginator(recipes, 2)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    ctxt = {"recipes": recipes, "categories": categories, "ingredients": ingredients, 'page_obj': page_obj}
 
     if request.is_ajax():
         html = render_to_string(
-            template_name="artists-results-partial.html",
-            context={"recipes": recipes}
+            template_name="render_many_posts.html",
+            context={"recipes": recipes, "page_obj": page_obj}
         )
 
         data_dict = {"html_from_view": html}
